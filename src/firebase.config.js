bcb68ae03e991b081effd1c157afb641a56ad221
Riyad0821 <mdriyad0821@gmail.com>
2020-09-19 20:03:17 +0600

    created project

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC8WsKd9yetLuNqEaGk7tQMjYbbsj3dh_A",
+    authDomain: "ema-john-simple-e14f7.firebaseapp.com",
+    databaseURL: "https://ema-john-simple-e14f7.firebaseio.com",
+    projectId: "ema-john-simple-e14f7",
+    storageBucket: "ema-john-simple-e14f7.appspot.com",
+    messagingSenderId: "683221724528",
+    appId: "1:683221724528:web:3eafee963938e6ac9c33b5"
+  };
+  export default firebaseConfig;
